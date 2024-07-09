a3f803a41f2fb00016d094c7249f173dac23ccf6
Ysaikumar5344 <168627478+Ysaikumar5344@users.noreply.github.com>
2024-07-09 13:23:57 +0530

    Project.py

--- Project.py
+++ Project.py
@@ -6,7 +6,7 @@ import pickle
 
 st.image(r'C:\Users\Y SAI KUMAR\Music\innomatics-footer-logo.webp')
 st.title("House Price Prediction")
-model=pickle.load(open(r"C:\Users\Y SAI KUMAR\New folder\lr.pkl","rb"))
+model=pickle.load(open(r"lr.pkl","rb"))
 SquareFeet=st.number_input("Enter the size of the house",min_value=600,max_value=5000,step=50)
 Bedrooms  = st.number_input("Enter the number of Bedrooms",min_value=0,max_value=5,step=1)
 Bathrooms = st.number_input("Enter the number of Bathrooms",min_value=0,max_value=5,step=1)
