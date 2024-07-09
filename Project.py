7250d3fc758ded5bf83330b91d2b6b6f15966626
Ysaikumar5344 <168627478+Ysaikumar5344@users.noreply.github.com>
2024-07-09 13:44:17 +0530

    Project.py

--- Project.py
+++ Project.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np 
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error,r2_score
 import pickle
 
 st.title("House Price Prediction")
